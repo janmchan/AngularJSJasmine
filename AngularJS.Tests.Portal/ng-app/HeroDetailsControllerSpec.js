a512127c17acb87875272f65b1fed9b5eb41db61
Jan Michael Chan <jan.m.chan@outlook.com>
2015-07-10 16:29:30 +1200

    fixed Jasmine Unit test and Removed packages

---
+++ AngularJS.Tests.Portal/ng-app/HeroDetailsControllerSpec.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="../../AngularJS.Portal/Scripts/angularjs/angular.js" />
+/// <reference path="../../AngularJS.Portal/Scripts/angularjs/angular-mocks.js" />
+/// <reference path="../../AngularJS.Portal/Scripts/Controllers/LandingPageController.js" />
+/// <reference path="../../AngularJS.Portal/Scripts/Controllers/HeroDetailsController.js" />
+/// <reference path="../../AngularJS.Portal/Scripts/AngularJSApp.js" />
+
+describe('Ctrl: HeroDetails', function () {
+    beforeEach(module('AngularJSApp'));
+
+    var HeroDetailsController, scope;
+    beforeEach(inject(function ($controller) {
+        scope = {};
+        HeroDetailsController = $controller('HeroDetailsController', {
+            $scope: scope
+        });
+    }));
+    it('should have scope defined', function () {
+        expect(scope).toBeDefined();
+    });
+    it('should have models defined', function () {
+        expect(scope.models).toBeDefined();
+    });
+    it('should have models.tab default to 1', function () {
+        expect(scope.models.tab).toBe(1);
+    });
+    it('selectTab should set tab to value', function () {
+        HeroDetailsController.selectTab(1);
+        expect(scope.models.tab).toBe(1);
+    });
+    it('isSelected should return if tab is selected', function () {
+        scope.models.tab = 3;
+        var isSelected = HeroDetailsController.isSelected(3);
+        expect(isSelected).toEqual(true);
+    });
+});
